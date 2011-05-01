6d82e506cafec95e74ceab4f497a2ad0fd454e66
theduke <reg@theduke.at>
2011-05-01 21:32:02 +0200

    Rewrote stuff so cronjob can do what it needs.

--- presentation.py
+++ presentation.py
@@ -7,86 +7,111 @@ import logging
 import os
 from fbstats import visual
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
+import os
+import smtplib
+import mimetypes
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.MIMEAudio import MIMEAudio
+from email.MIMEImage import MIMEImage
+from email.Encoders import encode_base64
 
-v = visual.FBStatsVisualizer()
+def removeNonAscii(self, s): return "".join(i for i in s if ord(i)<128)   
 
-path = '/var/www/dukestats/files/stats/'
+def sendMail(subject, text, gmailUser, gmailPassword, recipient):
+    msg = MIMEMultipart()
+    msg['From'] = gmailUser
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text))
+
+    mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmailUser, gmailPassword)
+    mailServer.sendmail(gmailUser, recipient, msg.as_string())
+    mailServer.close()
+
+
+def buildCharts(stats, visualizer, path, me):
+    html = '<html><body><h1>Facebook Statistics for %s</h1>' % me['name']
+
+    chart = visualizer.buildSexChart(stats.getFriendsSex())
+    chart.download(path + 'sex.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildRelatioshipStatusChart(stats.getFriendsRelationshipStatus())
+    chart.download(path + 'rel_status.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildCategorizedAgeChart(stats.getAgesCategorized())
+    chart.download(path + 'ages_cat.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildCategorizedPicCountChart(stats.getCategorizedPictureCounts())
+    chart.download(path + 'pic_count.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    ####visualizer.buildCategorizedTagCountChart(stats.getCategorizedTagCount()).download(path + 'tag_count.jpeg')
+    
+    chart = visualizer.buildAgePhotoCountScatterChart(stats.getAgeAndPicCounts(), (0, 2000), (10, 60))
+    chart.download(path + 'pic_count_age_scatter.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    chart = visualizer.buildAgePhotoCountScatterChart(stats.getAgeAndPicCounts(), (0, 1000), (20, 30))
+    chart.download(path + 'pic_count_age_scatter2.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getTagBuddies('me', True), 'Top 5 Tag Buddies')
+    chart.download(path + 'tag_buddies.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getWallPostersPostCount('me', True), 'Top 5 Posters on your Wall')
+    chart.download(path + 'wallposters.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildTagBuddiesWallPosterScatterChart(stats.getTagBuddyWallPosterCountSet('me'))
+    chart.download(path + 'tagbuddy_wallpost_scatter.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getInboxData('me', True)['received'], 'Top Private Message Senders')
+    chart.download(path + 'msg_senders.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getInboxData('me', True)['sent'], 'Top Private Message Recipients')
+    chart.download(path + 'msg_recipients.jpeg')
+    html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
+    
+    html += '</body></html>'
+    
+    path += 'stats.html'
+    file = open(path, 'w')
+    file.write(html.encode('ascii', 'replace'))
+    file.close()
 
 # JENNY ##code = 'yzPSvOdSpP2bnvYFuZKa8jVMzGTLvAPuR1rUG3bmukQ.eyJpdiI6IjRNbEVEY0d6M09uaEswUTJHcXI2THcifQ.9-UqJJNmZpvZe5TlkyFv9vUqSXcblSnxwQ3iV3Q5KlbSV_frE0-nB66Q94QtC8ccz9yX5WULQKW5aL9bW26nfwnc5fOLL8BzpEGxgW4vcAdmSLfxDoZKLfnnL9u8ZZyE'
 # sevim## 
 code = 'Wuq6rCmg4cW5SaBE5BolMwmqSGPnghvkmWIgxGOd7lY.eyJpdiI6ImhYRURqVTU0Y1ZYYUM0U1NXMnhMMlEifQ.XuF3roi2dOmfk7hANe9UAZfIvUK5ZFw1yiUzThKASL6ZI0Gzn-ec3Z7DwpZtZV425fTvlEoh-aUbnt01JbRZ9vg0HGadDKD7aXVlKAaELCn9uCtl8Gj5846ZGqZMIWDn'
 
-appId = '70894852433'
-appSecret = '2746461daf8cba7e727c32cb4832183c'
-redirect = 'http://dukestats.theduke.at/authorized'
+def buildUserStats(appId, appSecret, redirect, path, code):
+    fb = Facebook()
+    fb.initGraphByApp(appId, appSecret, code, redirect)
 
-fb = Facebook()
-fb.initGraphByApp(appId, appSecret, code, redirect)
-
-
-stats = FBStats()
-stats.setFb(fb)
-
-stats.getBaseData(True)
-
-visualizer = fbstats.visual.FBStatsVisualizer()
-
-me = stats._data['me']
-
-path += visualizer.removeNonAscii(me['name']).replace(' ', '').lower() + '/'
-
-if not os.path.exists(path):
-    os.makedirs(path)
+    stats = FBStats()
+    stats.setFb(fb)
     
-html = '<html><body><h1>Facebook Statistics for %s</h1>' % me['name']
+    stats.getBaseData(True)
+    
+    visualizer = fbstats.visual.FBStatsVisualizer()
+    
+    me = stats._data['me']
+    
+    path += visualizer.removeNonAscii(me['name']).replace(' ', '').lower() + '/'
+    
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+    buildCharts(stats, visualizer, path, me)
+    
+    return me
 
-chart = visualizer.buildSexChart(stats.getFriendsSex())
-chart.download(path + 'sex.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
 
-chart = visualizer.buildRelatioshipStatusChart(stats.getFriendsRelationshipStatus())
-chart.download(path + 'rel_status.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-chart = visualizer.buildCategorizedAgeChart(stats.getAgesCategorized())
-chart.download(path + 'ages_cat.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-chart = visualizer.buildCategorizedPicCountChart(stats.getCategorizedPictureCounts())
-chart.download(path + 'pic_count.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-####visualizer.buildCategorizedTagCountChart(stats.getCategorizedTagCount()).download(path + 'tag_count.jpeg')
-
-chart = visualizer.buildAgePhotoCountScatterChart(stats.getAgeAndPicCounts(), (0, 2000), (10, 60))
-chart.download(path + 'pic_count_age_scatter.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-chart = visualizer.buildAgePhotoCountScatterChart(stats.getAgeAndPicCounts(), (0, 1000), (20, 30))
-chart.download(path + 'pic_count_age_scatter2.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getTagBuddies('me', True), 'Top 5 Tag Buddies')
-chart.download(path + 'tag_buddies.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getWallPostersPostCount('me', True), 'Top 5 Posters on your Wall')
-chart.download(path + 'wallposters.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-chart = visualizer.buildTagBuddiesWallPosterScatterChart(stats.getTagBuddyWallPosterCountSet('me'))
-chart.download(path + 'tagbuddy_wallpost_scatter.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getInboxData('me', True)['received'], 'Top Private Message Senders')
-chart.download(path + 'msg_senders.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-chart = visualizer.buildTopRecordsPieChartFromSortedData(stats.getInboxData('me', True)['sent'], 'Top Private Message Recipients')
-chart.download(path + 'msg_recipients.jpeg')
-html += '<div class="chart-wrap"><img src="%s" /></div>' % chart.get_url()
-
-html += '</body></html>'
-
-path += 'stats.html'
-file = open(path, 'w')
-file.write(html.encode('ascii', 'replace'))
-file.close()
